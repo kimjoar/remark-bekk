2aa599e39d4286ebb6bec855e23e836290e4623b
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2011-12-04 19:16:51 +0100

    Moved JS to own file

---
+++ bekk.js
@@ -0,0 +1,8 @@
+var remark = this.remark;
+$(document).ready(function() {
+    remark.events.addListener("slidein", function() {
+      $("h1").not($("h1 + span")).wrapInner("<span>");
+      $(".pushed img").not($(".img > img")).parent().wrapInner("<span class='img'>")
+    });
+});
+
