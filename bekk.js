23bb94d7e477e6cb56603b14f050b92c4be2088d
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2011-12-10 14:24:21 +0100

    Initial support for columns

--- bekk.js
+++ bekk.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     remark.events.addListener("slidein", function() {
       $("h1").not($("h1 + span")).wrapInner("<span>");
       $(".pushed img").not($(".img > img")).parent().wrapInner("<span class='img'>")
+      $(".cols .col:last").addClass("last");
     });
 });
 
